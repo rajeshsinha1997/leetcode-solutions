e5e594c09d8e5d479c265245c8e9c231674794c7
Rajesh Sinha <rajeshsinha1997@gmail.com>
2025-06-23 03:02:09 +0530

    06-23-2025

--- solution-set-1_typescript/78.subsets.ts
+++ solution-set-1_typescript/78.subsets.ts
@@ -34,12 +34,49 @@ function backtrack(
   }
 }
 
+/**
+ * Generate all subsets of a given set of numbers using bitmasking
+ * @param nums an array of numbers for which we want to find all subsets
+ * @param result the array of all subsets
+ */
+function bitmask(nums: number[], result: number[][]) {
+  // calculate the total number of subsets
+  const totalNumberOfSubsets = 1 << nums.length;
+
+  // iterate through all possible subset numbers
+  for (let subsetInd = 0; subsetInd < totalNumberOfSubsets; subsetInd++) {
+    // create a new subset
+    const subset: number[] = [];
+
+    // variable to track the position of the set bit in the subset index
+    let bitPos = 0;
+
+    // iterate through each bit position in the subset index
+    while (bitPos < nums.length) {
+      // check if the bit at the current position is set
+      if (subsetInd & (1 << bitPos)) {
+        // if the bit is set, add the corresponding number to the subset
+        subset.push(nums[bitPos]);
+      }
+
+      // move to the next bit position
+      bitPos++;
+    }
+
+    // add the constructed subset to the result array
+    result.push(subset);
+  }
+}
+
 function subsets(nums: number[]): number[][] {
   // create a result array that will hold all subsets and add the empty set and the full set
   const result: number[][] = [];
 
   // start the backtracking process from index 0
-  backtrack(nums, 0, [], result);
+  // backtrack(nums, 0, [], result);
+
+  // alternatively, you can use the bitmasking approach
+  bitmask(nums, result);
 
   // return the result array containing all subsets
   return result;
